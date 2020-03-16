18d3109c48b9087c9ab6135d5d85cfcfd85db34d
Trenhob <54007903+Trenhob@users.noreply.github.com>
2020-03-16 16:28:39 -0400

    Added command line options and arguments

--- image_resizer.py
+++ image_resizer.py
@@ -1,29 +1,64 @@
 
 import os
 import sys
+import getopt
 from pathlib import Path
 
 from PIL import Image
 
-new_size = (1280, 720)  # width, height to resize to
+# Set Defaults
+USE_SUBDIRECTORIES = True
+INPUT_EXTENSION = ".png"
+OUTPUT_EXTENSION = ".jpg"
+WIDTH = 1280
+HEIGHT = 720
 myDir = os.getcwd()
+sysargs = sys.argv[1:]
 
-for root, dirs, files in os.walk(myDir): # TODO: Set command line options to specify dir
+try:
+    opts, args = getopt.getopt(sysargs, "ni:o:w:h:t:", ["inputextension=", "outputextension", "width=", "height=", "toplevel="])
+except getopt.GetoptError:
+    print("Usage: image_resizer.py -n -i inputextension -o outputextension -w width -h height -t toplevel_directory")
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt == '-n':
+        USE_SUBDIRECTORIES = False
+    if opt in ("-i", "--inputextension"):
+        INPUT_EXTENSION = str(arg)
+    if opt in ("-o", "--outputextension"):
+        OUTPUT_EXTENSION = str(arg)
+    if opt in ("-w", "--width"):
+        WIDTH = int(arg)
+    if opt in ("-h", "--height"):
+        HEIGHT = int(arg)
+    if opt in ("-t", "--toplevel"): # TODO: Fix spaces in filepaths breaking stuff
+        myDir = arg
+
+
+new_size = (WIDTH, HEIGHT)  # width, height for resized output
+
+for root, dirs, files in os.walk(myDir):
     
     for file in files:        
-        if file.endswith(".png"):
+        if file.endswith(INPUT_EXTENSION):
             full_path = os.path.join(root, file)
-            outfile = os.path.splitext(file)[0] + ".jpg"  # rename to jpeg
-            processedPath = Path(f"{root}/{new_size[0]}x{new_size[1]}") # full path required
-            processedPath.mkdir(parents=True, exist_ok=True) # create subfolder
+            outfile = os.path.splitext(file)[0] + OUTPUT_EXTENSION  # rename
+            
+
+            if USE_SUBDIRECTORIES:
+                processedPath = Path(f"{root}/{new_size[0]}x{new_size[1]}") # full path required
+                processedPath.mkdir(parents=True, exist_ok=True) # create subfolder
+            else:
+                processedPath = Path(f"{root}")
 
             if file != outfile:
                  
                 try:
                     with Image.open(full_path) as im:
                         im = im.resize(new_size)  # set new size
-                        im.save(os.path.join(processedPath, outfile), "JPEG") # save as jpeg
+                        im.save(os.path.join(processedPath, outfile)) # save as jpeg
                         print(f"Saved {outfile} at {im.size[0]} x {im.size[1]} pixels.")
                         
                 except IOError:
-                    print(f"Cannot create jpeg for {file}. It may already exist.")
+                    print(f"Cannot resize {file}.")
